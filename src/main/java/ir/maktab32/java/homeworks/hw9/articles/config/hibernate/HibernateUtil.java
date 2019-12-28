681b625a10ae011bbfedc2eea8d7b3f82fffccb5
arfjava <arrfjava@gmail.com>
2019-12-28 08:19:41 +0330

    HibernateUtil.java created and getSession method provided

---
+++ src/main/java/ir/maktab32/java/homeworks/hw9/articles/config/hibernate/HibernateUtil.java
@@ -0,0 +1,17 @@
+package ir.maktab32.java.homeworks.hw9.articles.config.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+    private static Session session;
+
+    static {
+        sessionFactory = new Configuration().configure().buildSessionFactory();
+        session = sessionFactory.openSession();
+    }
+
+    public static Session getSession(){return session;}
+}
