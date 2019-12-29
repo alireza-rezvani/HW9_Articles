69200a1331b1148be692a94d83eb1477602ab258
arfjava <arrfjava@gmail.com>
2019-12-29 14:03:15 +0330

    ChangePasswordByUserUseCase.java and ChangeUserRoleByAdminUseCase.java edited

--- src/main/java/ir/maktab32/java/homeworks/hw9/articles/features/usermanagement/usecase/ChangePasswordUseCase.java
+++
@@ -1,4 +0,0 @@
-package ir.maktab32.java.homeworks.hw9.articles.features.usermanagement.usecase;
-
-public interface ChangePasswordUseCase {
-}
--- src/main/java/ir/maktab32/java/homeworks/hw9/articles/features/usermanagement/usecase/ChangeUserRoleByAdminUseCase.java
+++ src/main/java/ir/maktab32/java/homeworks/hw9/articles/features/usermanagement/usecase/ChangeUserRoleByAdminUseCase.java
@@ -1,4 +1,8 @@
 package ir.maktab32.java.homeworks.hw9.articles.features.usermanagement.usecase;
 
+import ir.maktab32.java.homeworks.hw9.articles.entities.Role;
+import ir.maktab32.java.homeworks.hw9.articles.entities.User;
+
 public interface ChangeUserRoleByAdminUseCase {
+    boolean execute(User user, Role newRole);
 }
